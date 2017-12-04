fc9851c6d2f42fd4b06c9928505bf709abe123b7
Brandon Watts <brandonwatts@outlook.com>
2017-12-03 22:32:01 -0500

    Add new fields to front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,7 +8,7 @@ export default class Site extends Component {
 
     constructor(props) {
     super(props);
-    this.state = {min_sqft: '', max_sqft: '', min_bdrm:'', max_bdrm:'', min_price:'', max_price:'',  price_type:'',zip_code:'',address:'',city:''};
+    this.state = {min_sqft: '', max_sqft: '', min_bdrm:'', max_bdrm:'', min_price:'', max_price:'',  price_type:'',zip_code:'',address:'', city:'', us_state:'', build_year:''};
   }
 
     loadResults(request){
@@ -18,7 +18,7 @@ export default class Site extends Component {
                 this.setState({min_sqft:response.data.min_sqft, max_sqft: response.data.max_sqft, min_bdrm: response.data.min_bed,
                                max_bdrm: response.data.max_bed, min_price: response.data.min_price, max_price: response.data.max_price,
                                price_type: response.data.pricing_type, zip_code: response.data.zip_code, address: response.data.address,
-                               city: response.data.city})
+                               city: response.data.city, us_state: response.data.state, build_year: response.data.build_year})
             })
             .catch( (error) => {
                 console.log(error);
@@ -43,7 +43,8 @@ export default class Site extends Component {
                     </div>
                 </section>
                 <Table min_sqft={this.state.min_sqft} max_sqft={this.state.max_sqft} min_bdrm={this.state.min_bdrm} max_bdrm={this.state.max_bdrm} min_price={this.state.min_price}
-                        max_price={this.state.max_price} price_type={this.state.price_type} zip_code={this.state.zip_code} address={this.state.address} city={this.state.city} />
+                        max_price={this.state.max_price} price_type={this.state.price_type} zip_code={this.state.zip_code} address={this.state.address} city={this.state.city} us_state={this.state.us_state}
+                        build_year={this.state.build_year}/>
             </div>
         );
     }
--- front-end/src/Table.js
+++ front-end/src/Table.js
@@ -16,33 +16,51 @@ export default class Table extends Component{
             </div>
             <div className="row">
               <div className="col-12">
-                <table className="table table-hover">
+                <table className="table">
                   <thead>
                     <tr>
-                      <th>Minimum Square Feet</th>
-                      <th>Maximum Square Feet</th>
                       <th>Minimum Bedrooms</th>
                       <th>Maximum Bedrooms</th>
                       <th>Minimum Price</th>
                       <th>Maximum Price</th>
+                      <th>City</th>
+                      <th>State</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>{this.props.min_bdrm}</td>
+                      <td>{this.props.max_bdrm}</td>
+                      <td>{this.props.min_price}</td>
+                      <td>{this.props.max_price}</td>
+                      <td>{this.props.city}</td>
+                      <td>{this.props.us_state}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th>Minimum Square Feet</th>
+                      <th>Maximum Square Feet</th>
+                      <th>Build Year</th>
                       <th>Pricing Type</th>
                       <th>Zip Code</th>
                       <th>Address</th>
-                      <th>City</th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
                       <td>{this.props.min_sqft}</td>
                       <td>{this.props.max_sqft}</td>
-                      <td>{this.props.min_bdrm}</td>
-                      <td>{this.props.max_bdrm}</td>
-                      <td>{this.props.min_price}</td>
-                      <td>{this.props.max_price}</td>
+                      <td>{this.props.build_year}</td>
                       <td>{this.props.price_type}</td>
                       <td>{this.props.zip_code}</td>
                       <td>{this.props.address}</td>
-                      <td>{this.props.city}</td>
                     </tr>
                   </tbody>
                 </table>
