348263fadb98dd71d0d39be0d73751bd2baa7d3a
Joshua Stickles <jstickles@RICJSTICKLES1.local>
2018-03-29 12:46:47 -0400

    cleaned up a few small things

--- front-end/src/ApartmentCard.jsx
+++ front-end/src/ApartmentCard.jsx
@@ -1,17 +1,25 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 export default class ApartmentCard extends Component {
-
   render() {
     return (
       <div className="col-4">
-      <div className="card mt-3">
-      <img className="card-img-top img" src={this.props.apartment.PropertyPhotoUrl} alt="Card image cap"/>
-      <div className="card-block">
-        <h4 className="card-title">{this.props.apartment.PropertyName}</h4>
-        <p className="card-text mb-0"><i className="fa fa-map-marker mr-1"></i>{this.props.apartment.Listing.Address.City}, {this.props.apartment.Listing.Address.State}</p>
+        <div className="card mt-3">
+          <img
+            className="card-img-top img"
+            src={this.props.apartment.PropertyPhotoUrl}
+            alt={this.props.apartment.PropertyName}
+          />
+          <div className="card-block">
+            <h4 className="card-title">{this.props.apartment.PropertyName}</h4>
+            <p className="card-text mb-0">
+              <i className="fa fa-map-marker mr-1" />
+              {this.props.apartment.Listing.Address.City},{" "}
+              {this.props.apartment.Listing.Address.State}
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>);
+    );
   }
-};
+}
