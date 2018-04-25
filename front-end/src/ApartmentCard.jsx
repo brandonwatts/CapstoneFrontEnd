b9da48ead4a90f621881d7b7e4444ad88a67aea1
Brandon Watts <brandonwatts@outlook.com>
2018-04-25 14:12:29 -0400

    Remove unmapped fields

--- front-end/src/ApartmentCard.jsx
+++ front-end/src/ApartmentCard.jsx
@@ -49,37 +49,10 @@ export default class ApartmentCard extends Component {
               </div>
               <div className="col-6">
                 <p className="card-text text-right">
-                  <i className="fa fa-bath mr-1" />
-                  {this.props.apartment.RentFormat.Beds}
-                </p>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-6">
-                <p className="card-text mb-1">
-                  <i className="fa fa-arrows-alt mr-1" />
-                  {this.props.apartment.RentFormat.Beds}
-                </p>
-              </div>
-              <div className="col-6">
-                <p className="card-text text-right">
                   {this.getNumStars(this.props.apartment.Listing.Rating)}
                 </p>
               </div>
             </div>
-          </div>
-          <div className="card-footer">
-            <i className="fas fa-paw mr-2" />
-            <i className="fas fa-wheelchair mr-2" />
-            <i className="fae-shirt mr-2" />
-            <i className="fas fa-couch mr-2" />
-            <i className="fas fa-tint mr-2" />
-            <i className="fas fa-car mr-2" />
-            <i className="fas fa-caret-square-up mr-2" />
-            <i className="fab fa-github-alt mr-2" />
-            <i className="fas fa-coffee mr-2" />
-            <i className="fas fa-snowflake mr-2" />
-            <i className="fae-halter mr-2" />
           </div>
         </div>
       </div>
